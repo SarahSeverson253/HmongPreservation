adb457b8b87360708ad51ef1f46009ab187d32d2
SarahSeverson253 <30361797+SarahSeverson253@users.noreply.github.com>
2018-03-17 16:24:09 -0500

    Add files via upload
    Full registration, login, profile, and password recovery system. Image upload to be added to profile soon.

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+/* Database connection settings */
+$host = 'sql206.byethost9.com';
+$user = 'b9_21611491';
+$pass = 'sh33ph3ad';
+$db = 'b9_21611491_hmongPres';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
